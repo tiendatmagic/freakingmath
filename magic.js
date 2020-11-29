c3d0620f0f22e04ce3694c39c6b8556680054af5
tiendatmagic <tiendatmagic8@gmail.com>
2020-11-29 14:52:57 +0700

    updatecode

--- magic.js
+++ magic.js
@@ -1,57 +1,12 @@
-
 var score = 0;
-var highscore = 0;
 var time = 60;
 var tim;
-
-
-function setCookie(hihihi, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000000));
-    //	d.setTime(d.getTime() + (exdays * 48 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = hihihi + "=" + cvalue + ";" + expires + ";path=/";
+var highscore = JSON.parse(localStorage.getItem("highscore"));
+if (highscore === null) {
+    highscore = 0;
 }
-
-function getCookie(hihihi) {
-    var name = hihihi + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var fhighscore = getCookie("fhighscore");
-    if (fhighscore != "") {
-        highscore = Number(fhighscore);
-    } else {
-        if (fhighscore != null) {
-            setCookie("fhighscore", fhighscore, 30);
-        }
-    }
-
-    document.getElementsByClassName("highscore")[0].innerHTML = "Điểm cao nhất:" + highscore;
-    var fhighscore = getCookie("fhighscore");
-    fhighscore = highscore;
-    if (fhighscore != null) {
-        setCookie("fhighscore", fhighscore, 30);
-
-    }
-    document.getElementsByClassName("highscore")[0].innerHTML = "Điểm cao nhất:" + highscore;
-}
-
+document.getElementsByClassName("highscore")[0].innerHTML = "Điểm cao nhất:" + highscore;
 document.getElementsByClassName("btn-start")[0].addEventListener("click", function () {
-
-
     document.getElementsByClassName("score")[0].innerHTML = "Điểm :" + score;
     document.getElementsByClassName("footer")[0].style.display = 'none';
     document.getElementById("mainstart").style.display = 'none';
@@ -60,35 +15,21 @@ document.getElementsByClassName("btn-start")[0].addEventListener("click", functi
     startgame();
     checktime();
     document.getElementsByClassName("highscore")[0].innerHTML = "Điểm cao nhất:" + highscore;
-    var fhighscore = getCookie("fhighscore");
-    fhighscore = highscore;
-    if (fhighscore != null) {
-        setCookie("fhighscore", fhighscore, 30);
-
-    }
     document.getElementsByClassName("highscore")[0].innerHTML = "Điểm cao nhất:" + highscore;
 });
-
-
-
 document.getElementsByClassName("btn-end")[0].addEventListener("click", function () {
     location.reload();
-
 });
-
 document.getElementsByClassName("btn-reload")[0].addEventListener("click", function () {
     location.reload();
-
 });
 document.getElementsByClassName("true")[0].addEventListener("click", truee);
 document.getElementsByClassName("false")[0].addEventListener("click", falsee);
 
 function startgame() {
-    
     a = Math.floor(Math.random() * 50);
     b = Math.floor(Math.random() * 50);
-    rd = Math.floor(Math.random() * 2);
-
+    rd = Math.floor(Math.random() * 3);
     switch (rd) {
         case 0:
             c = a + b + Math.ceil(Math.random() * 4);
@@ -98,8 +39,12 @@ function startgame() {
             c = a + b;
             ques = 1;
             break;
-
+        case 2:
+            c = a + b + 10;
+            ques = 0;
+            break;
         default:
+            rd = Math.floor(Math.random() * 3);
             break;
     }
     document.getElementsByClassName("s1")[0].innerText = a;
@@ -113,34 +58,22 @@ function checktime() {
         if (time <= 0) {
             time = 0;
             clearInterval(tim);
-
         }
-
         if (time === 0) {
             document.getElementsByClassName("score")[0].innerHTML = "Điểm: " + score;
             document.getElementsByClassName("modal")[0].style.display = 'block';
             document.getElementsByClassName("score")[1].innerHTML = "Điểm: " + score;
         }
     }, 1000);
-
-
-
-
 }
-function truee() {
 
+function truee() {
     answ = 1;
     if (ques === answ) {
         score += 1;
         document.getElementsByClassName("score")[0].innerText = "Điểm: " + score;
-
-
-
         startgame();
-
-    }
-   
-    else {
+    } else {
         document.getElementsByClassName("score")[0].innerHTML = "Điểm: " + score;
         document.getElementsByClassName("modal")[0].style.display = 'block';
         document.getElementsByClassName("score")[1].innerHTML = "Điểm: " + score;
@@ -155,15 +88,9 @@ function falsee() {
     if (ques === answ) {
         score += 1;
         document.getElementsByClassName("score")[0].innerText = "Điểm: " + score;
-
-
         startgame();
-
-    }
-
-    else {
+    } else {
         document.getElementsByClassName("score")[0].innerHTML = "Điểm :" + score;
-        //location.reload();
         document.getElementsByClassName("modal")[0].style.display = 'block';
         document.getElementsByClassName("score")[1].innerHTML = "Điểm: " + score;
         time = 0;
@@ -172,20 +99,12 @@ function falsee() {
     savehighscore();
 }
 
-
-
 function savehighscore() {
     if (score > highscore) {
         highscore = score;
         document.getElementsByClassName("highscore")[0].innerText = "Điểm: " + highscore;
         document.getElementsByClassName("highscore")[0].innerHTML = "Điểm cao nhất:" + highscore;
-        var fhighscore = getCookie("fhighscore");
-        fhighscore = highscore;
-        if (fhighscore != null) {
-            setCookie("fhighscore", fhighscore, 30);
-
-        }
+        localStorage.setItem("highscore", JSON.stringify(highscore));
         document.getElementsByClassName("highscore")[0].innerHTML = "Điểm cao nhất:" + highscore;
-        
     }
 }
