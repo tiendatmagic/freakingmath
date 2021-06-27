d60538a296eacd1a3129b1feb161fc2cdf3c06c9
tiendatmagic <tiendatmagic8@gmail.com>
2021-06-27 21:11:13 +0700

    update

--- magic.js
+++ magic.js
@@ -31,23 +31,20 @@ document
       "Điểm cao nhất:" + highscore;
     document.getElementsByClassName("highscore")[0].innerHTML =
       "Điểm cao nhất:" + highscore;
-    bgcolor = Math.floor(Math.random() * 4);
+    bgcolor = Math.floor(Math.random() * 3);
 
     switch (bgcolor) {
       case 0:
-        document.getElementById("main").style.backgroundColor = "#c92bc9";
+        document.getElementById("main").style.color = "#c92bc9";
         break;
       case 1:
-        document.getElementById("main").style.backgroundColor = "#FF66FF";
+        document.getElementById("main").style.color = "#0099FF";
         break;
       case 2:
-        document.getElementById("main").style.backgroundColor = "#0099FF";
-        break;
-      case 3:
-        document.getElementById("main").style.backgroundColor = "#FF6633";
+        document.getElementById("main").style.color = "#FF6633";
         break;
       default:
-        document.getElementById("main").style.backgroundColor = "purple";
+        document.getElementById("main").style.color = "purple";
         break;
     }
   });
